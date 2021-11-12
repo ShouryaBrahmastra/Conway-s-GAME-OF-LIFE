1f73a50e7fff7456a4cdf7225a4b479dbe553ca2
Shourya Gupta <gptshourya02@gmail.com>
2021-11-12 12:44:08 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,39 @@
+# Conway-s-GAME-OF-LIFE---Python
+
+An implementation of [Conway's Game of Life](https://en.wikipedia.org/wiki/Conway's_Game_of_Life)
+
+# Follow me on Github [ShouryaBrahmastra](https://github.com/ShouryaBrahmastra)
+
+---
+
+## Rules
+
+"Black colour" : Death
+"White colour" : Life
+
+The box of pixels (cells) of the screen represent the cells in the Game of Life. Clicking a cell will turn it black. White cells represent "living" cells.
+Living and dead cells will change over time, following the rules of Conway's Game of Life:
+
+1.>Any live cell with two or three live neighbours survives.
+2.>Any dead cell with three live neighbours becomes a live cell.
+3.>All other live cells die in the next generation. Similarly, all other dead cells stay dead.
+
+---
+
+### Requirements
+
+> **Pygame** : pip install pygame
+
+> **Numpy** : pip install numpy
+
+---
+
+### COMMANDS ON THE SCREEN AFTER COMPILATION
+
+        "Esc"             : to close the window
+        "Space"           : to Pause the simulation
+        "Mouse Left Click": to draw on the grid
+
+---
+
+Show some ❤❤ love ❤❤ by giving some Star to this repo.
---
+++ main.py
@@ -0,0 +1,58 @@
+# Importing modules
+import pygame as P_Game
+import numpy as np
+import os
+import grid
+
+# Environment configuration
+os.environ["SDL_VIDEO_CENTERED"] = '1'
+
+# resolution
+width, height = 1920, 1080
+size = (width, height)
+
+
+P_Game.init()
+P_Game.display.set_caption("CONWAY'S GAME OF LIFE")
+screen = P_Game.display.set_mode(size)
+clock = P_Game.time.Clock()
+fps = 20
+
+black = (0, 0, 0)
+blue = (0, 121, 150)
+blue1 = (0, 14, 71)
+white = (255, 255, 255)
+
+scaler = 30
+offset = 1
+
+Grid = grid.Grid(width, height, scaler, offset)
+Grid.random2d_array()
+
+
+pause = False
+run = True
+
+while run:
+    clock.tick(fps)
+    screen.fill(black)
+
+    # Commands
+    for event in P_Game.event.get():
+        if event.type == P_Game.QUIT:
+            run = False
+        if event.type == P_Game.KEYUP:
+            if event.key == P_Game.K_ESCAPE:
+                run = False
+            if event.key == P_Game.K_SPACE:
+                pause = not pause
+
+    Grid.Conway(off_color=white, on_color=blue1, surface=screen, pause=pause)
+
+    if P_Game.mouse.get_pressed()[0]:
+        mouseX, mouseY = P_Game.mouse.get_pos()
+        Grid.HandleMouse(mouseX, mouseY)
+
+    P_Game.display.update()
+
+P_Game.quit()
